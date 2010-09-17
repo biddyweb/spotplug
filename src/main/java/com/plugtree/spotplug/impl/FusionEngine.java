3e9a9f9b577868b34c6771136065aecafab8395a
Nico <Nico@Nico-PC>
2010-09-17 16:28:56 -0300

    Implementation of a Global Variable a hashMap for Users inside de Drl File

--- src/main/java/com/plugtree/spotplug/impl/FusionEngine.java
+++ src/main/java/com/plugtree/spotplug/impl/FusionEngine.java
@@ -63,6 +63,8 @@ public class FusionEngine implements Engine {
 			User user = new User(userId);
 			hashMap.put(userId, user);
 			
+			session.setGlobal("hashMap",hashMap);
+			
 			session.insert(user);
 		}
 	}
