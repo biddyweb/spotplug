1422912ea528034ac0e196b976742fd23b4f371e
Leandro-Romero <leandro.s.romero@gmail.com>
2010-09-21 07:59:19 -0300

    More test, although not automatic yet.
    Improve in fraud pattern number 5.

--- src/test/java/RulesTest.java
+++ src/test/java/RulesTest.java
@@ -28,10 +28,51 @@ public class RulesTest extends TestCase {
 	
 	public void testExcesiveAmountPattern() {
 		
-		GenericEvent event1 = new GenericEvent("leandro", 6000, new Date(), 20000, 0, 1);
-		GenericEvent event2 = new GenericEvent("leandro", 5000, new Date(), 20000, 0, 1);
-		GenericEvent event3 = new GenericEvent("leandro", 10001, new Date(), 20000, 0, 1);
+		GenericEvent event1 = new GenericEvent("Zeus", 6000, new Date(), 20000, 0, 1);
+		GenericEvent event2 = new GenericEvent("Zeus", 5000, new Date(), 20000, 1, 2);
+		GenericEvent event3 = new GenericEvent("Zeus", 10001, new Date(), 20000, 3, 3);
 		
 		engine.processEvent(event1);
+		//No se activa
+		engine.processEvent(event2);
+		//No se activa
+		engine.processEvent(event3);
+		//Se activa	
 	}
+	
+	public void testManyEventsShortPeriod() {
+		GenericEvent event1 = new GenericEvent("Thor", 6000, new Date(), 20000, 0, 1);
+		GenericEvent event2 = new GenericEvent("Thor", 5000, new Date(), 20000, 1, 2);
+		GenericEvent event3 = new GenericEvent("Thor", 10001, new Date(), 20000, 2, 3);
+		GenericEvent event4 = new GenericEvent("Thor", 10001, new Date(), 20000, 3, 4);
+		
+		engine.processEvent(event1);
+		//No se activa
+		engine.processEvent(event2);
+		//No se activa
+		engine.processEvent(event3);
+		//No se activa
+		engine.processEvent(event4);
+		//Se activa
+	}
+	
+	public void testIdenticalTransactions() {
+		GenericEvent event1 = new GenericEvent("Hera", 6000, new Date(), 20000, 0, 1);
+		GenericEvent event2 = new GenericEvent("Hulk", 6000, new Date(), 20000, 1, 2);
+		GenericEvent event3 = new GenericEvent("Hera", 7000, new Date(), 20000, 2, 3);
+		GenericEvent event4 = new GenericEvent("Hera", 6000, new Date(), 20000, 3, 4);
+		
+		engine.processEvent(event1);
+		//No se activa
+		
+		engine.processEvent(event2);
+		//No se activa
+		
+		engine.processEvent(event3);
+		//No Se activa
+		
+		engine.processEvent(event4);
+		//Se activa
+	}
+	
 }
