c5bbda5079782ffcec1c8573cc3baacf13385b8e
Leandro-Romero <leandro.s.romero@gmail.com>
2010-09-27 13:56:04 -0300

    Junit version updated. Hornetq dependencies added.
    Empty hornetQtest added.

---
+++ src/test/resources/HornetQTest.java
@@ -0,0 +1,17 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+public class HornetQTest {
+
+	@BeforeClass
+	public void inicialize() {
+		
+	}
+	
+	@AfterClass() 
+	public void finalize() {
+		
+	}
+	
+	
+}
