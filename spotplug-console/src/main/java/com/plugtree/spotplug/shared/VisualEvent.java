5fd16e9b840a4b8aa8960cb2643e2b84fa6dc708
Nicolas Sanchez <sancheznicolas89@gmail.com>
2011-05-13 13:39:36 +0000

    RPC Class Models added

---
+++ spotplug-console/src/main/java/com/plugtree/spotplug/shared/VisualEvent.java
@@ -0,0 +1,39 @@
+package com.plugtree.spotplug.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class VisualEvent implements IsSerializable {
+    private String eventName;
+    private long timestamp;
+    private Map<String,String> attributesMap = new HashMap<String,String>();
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void addAttribute(String key,String value){
+         attributesMap.put(key,value);
+
+    }
+
+    public String getAttribute(String key){
+
+    }
+
+}
---
+++ spotplug-console/src/main/java/com/plugtree/spotplug/shared/VisualRule.java
@@ -0,0 +1,27 @@
+package com.plugtree.spotplug.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import java.util.HashMap;
+
+
+public class VisualRule implements IsSerializable {
+    private String ruleName;
+    private long timestamp;
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
