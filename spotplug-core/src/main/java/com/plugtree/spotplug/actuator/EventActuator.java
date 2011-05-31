43ba03bc2f00b383ee7c451e9b59e7f6ea34d3b8
Nicolas Sanchez <sancheznicolas89@gmail.com>
2011-05-31 08:25:33 +0000

    EventActuator now has reference to the bus, and can insert events in it.

--- spotplug-core/src/main/java/com/plugtree/spotplug/actuator/EventActuator.java
+++ spotplug-core/src/main/java/com/plugtree/spotplug/actuator/EventActuator.java
@@ -1,11 +1,24 @@
 package com.plugtree.spotplug.actuator;
 
+import com.plugtree.spotplug.bus.Bus;
+import com.plugtree.spotplug.model.GenericEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import java.util.LinkedList;
 import java.util.List;
 
 
 public class EventActuator {
    private LinkedList<String> eventNames = new LinkedList<String>();
+   private ApplicationContext context;
+   private Bus bus;
+
+   void EventActuator(){
+       context = new ClassPathXmlApplicationContext(new String[] {"/core.xml"});
+       bus = (Bus) context.getBean("Bus");
+   }
+
 
    public void addEventName(String eventName){
           if(!eventNames.contains(eventName)){
@@ -15,6 +28,11 @@ public class EventActuator {
 
     public List<String> getEventNames(){
       return eventNames;
+    }
+
+     public void insertEventInBus(GenericEvent event){
+       bus.addEvent(event); 
+
     }
 
 }
