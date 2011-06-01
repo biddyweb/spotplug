d339f7d54f5d65356f14851b2648b7e7054e8b6f
Leandro Romero <leandro.s.romero@gmail.com>
2011-06-01 10:09:46 -0300

    EventActuator improvement

--- spotplug-core/src/main/java/com/plugtree/spotplug/actuator/EventActuator.java
+++ spotplug-core/src/main/java/com/plugtree/spotplug/actuator/EventActuator.java
@@ -1,38 +1,36 @@
 package com.plugtree.spotplug.actuator;
 
-import com.plugtree.spotplug.bus.Bus;
-import com.plugtree.spotplug.model.GenericEvent;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.util.LinkedList;
 import java.util.List;
 
+import com.plugtree.spotplug.bus.Bus;
+import com.plugtree.spotplug.model.GenericEvent;
+
 
 public class EventActuator {
-   private LinkedList<String> eventNames = new LinkedList<String>();
-   private ApplicationContext context;
-   private Bus bus;
 
-   void EventActuator(){
-       context = new ClassPathXmlApplicationContext(new String[] {"/core.xml"});
-       bus = (Bus) context.getBean("Bus");
-   }
+	private LinkedList<String> eventNames = new LinkedList<String>();
+	private Bus bus;
 
+	public void addEventName(String eventName) {
+		if(!eventNames.contains(eventName)) {
+			eventNames.add(eventName);
+		}
+	}
 
-   public void addEventName(String eventName){
-          if(!eventNames.contains(eventName)){
-                 eventNames.add(eventName);
-          }
-   }
+	public List<String> getEventNames(){
+		return eventNames;
+	}
 
-    public List<String> getEventNames(){
-      return eventNames;
-    }
+	public void insertEventInBus(GenericEvent event){
+		bus.addEvent(event);
+	}
 
-     public void insertEventInBus(GenericEvent event){
-       bus.addEvent(event); 
+	public void setBus(Bus bus) {
+		this.bus = bus;
+	}
 
-    }
-
+	public Bus getBus() {
+		return bus;
+	}
 }
